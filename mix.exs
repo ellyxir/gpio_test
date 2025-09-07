e3804809d1512920a8c004947958a253266f6b9d
Ellyse <ellyse7777@gmail.com>
2025-09-07 02:59:09 +0200

    use rebuilt kernel that has sound driver included

--- mix.exs
+++ mix.exs
@@ -64,18 +64,19 @@ defmodule HelloNerves.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
-      {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
-      {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
-      {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
-      {:nerves_system_rpi3a, "~> 1.24", runtime: false, targets: :rpi3a},
-      {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
-      {:nerves_system_rpi5, "~> 0.2", runtime: false, targets: :rpi5},
-      {:nerves_system_bbb, "~> 2.19", runtime: false, targets: :bbb},
-      {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
-      {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
-      {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
-      {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
+      # {:nerves_system_rpi, "~> 1.24", runtime: false, targets: :rpi},
+      # {:nerves_system_rpi0, "~> 1.24", runtime: false, targets: :rpi0},
+      # {:nerves_system_rpi2, "~> 1.24", runtime: false, targets: :rpi2},
+      # {:nerves_system_rpi3, "~> 1.24", runtime: false, targets: :rpi3},
+      # Use custom system with USB audio support
+      {:nerves_system_rpi3, path: System.get_env("HOME") <> "/projects/custom_rpi3", runtime: false, targets: :rpi3},
+      # {:nerves_system_rpi4, "~> 1.24", runtime: false, targets: :rpi4},
+      # {:nerves_system_rpi5, "~> 0.2", runtime: false, targets: :rpi5},
+      # {:nerves_system_bbb, "~> 2.19", runtime: false, targets: :bbb},
+      # {:nerves_system_osd32mp1, "~> 0.15", runtime: false, targets: :osd32mp1},
+      # {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
+      # {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
+      # {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro}
     ]
   end
 
